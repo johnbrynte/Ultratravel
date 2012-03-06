506857e3c8bf6272a7f2baeb64f694b8434e2dd9
Anton Erholt <aerholt@kth.se>
2012-03-06 22:17:45 +0100

    improved javascript ALOT (made it pass JSLint.. sort of)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*jslint undef: true, browser: true, unparam: true, sloppy: true, white: true, newcap: true, plusplus: true, maxerr: 50, indent: 4 */
+/*jslint browser: true, sloppy: true, maxerr: 50, indent: 4 */
 
 /**
  * Script.js
@@ -30,17 +30,19 @@ var selectedFlight = {};
 var currentHotels;
 var selectedHotel = {};
 
-$(document).ready(function() {
-    var today = new Date();
-    var month = today.getMonth();
-    var date = today.getDate();
-    var todayString = today.getFullYear()+"/"+
-        ((month<9)?"0"+(month+1):month+1)+"/"+
-        ((date<10)?"0"+date:date)+" 13:37";
-    var $sectionHeaders = $(".section > h1");
+$(document).ready(function () {
+    var today, month, date, todayString, $sectionHeaders,myOptions, fa, ta;
+
+    today = new Date();
+    month = today.getMonth();
+    date = today.getDate();
+    todayString = today.getFullYear() + "/" +
+        ((month<9) ? "0" + (month+1) : month + 1 ) + "/" +
+        ((date<10) ? "0" + date : date ) + " 13:37";
+    $sectionHeaders = $(".section > h1");
 
     // GOOGLE MAPZ
-    var myOptions = {
+    myOptions = {
         disableDefaultUI: true,
         scrollwheel: false,
         center: new google.maps.LatLng(50, 0),
@@ -65,8 +67,8 @@ $(document).ready(function() {
     $(".section > h1").css("background",todoColor);
 
     // set the click event handler for each section
-    $sectionHeaders.click(function() {
-        if($(this).is(".enabled")) { // literally, baby!!!
+    $sectionHeaders.click(function () {
+        if ($(this).is(".enabled")) { // literally, baby!!!
             gotoSection($(".section").index($(this).parent())+1);
         }
     });
@@ -76,41 +78,41 @@ $(document).ready(function() {
 
     // CALENDAR
     $("#arrival").val(todayString);
-    $("#departure").val(todayString)
+    $("#departure").val(todayString);
 
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
     geocoder = new google.maps.Geocoder();
 
     // FORM
-    var fa = $("#fromaddress");
-    var ta = $("#toaddress");
-    fa.keypress(function(event) {
+    fa = $("#fromaddress");
+    ta = $("#toaddress");
+    fa.keypress(function (event) {
         if (event.which === 13) { // check if enter was pressed
             codeAddress(0, $(this).attr("value"));
         }
-    }).focusout(function(event) {
+    }).focusout(function () {
         if (fa.val() !== '') {
             codeAddress(0, $(this).attr("value"));
         }
     });
 
 
-    ta.keypress(function(event) {
+    ta.keypress(function (event) {
         if (event.which === 13) { // check if enter was pressed
             codeAddress(1, $(this).attr("value"));
         }
-    }).focusout(function(event) {
-        if (ta.val()!== '') {
+    }).focusout(function () {
+        if (ta.val() !== '') {
             codeAddress(1, $(this).attr("value"));
         }
     });
 
     // If the text field in section 1 changed
-    ta.keyup(function(event) {
+    ta.keyup(function (event) {
         firstSectionChange();
     });
-    fa.keyup(function(event) {
+    fa.keyup(function (event) {
         firstSectionChange();
     });
 
@@ -118,7 +120,7 @@ $(document).ready(function() {
     fa.focus();
 
     // LOGIN
-    if (typeof(Storage)!=="undefined") {
+    if (typeof (Storage) !=="undefined") {
         saveFunc = saveDataLocal;
         loadFunc = loadDataLocal;
     } else {
@@ -133,12 +135,12 @@ $(document).ready(function() {
     if (ultratravelUserData && ultratravelUserData.loggedin) {
         writeLoginInfo();
     } else {
-        $('#login').append('<div id="login_popup">'
-            + '<span>Anv&auml;ndarnamn: <input id="login_username" type="text" /></span>'
-            + '<span>L&ouml;senord: <input id="login_password" type="password" /></span>'
-            + '</div>');
+        $('#login').append('<div id="login_popup">' +
+            '<span>Anv&auml;ndarnamn: <input id="login_username" type="text" /></span>' +
+            '<span>L&ouml;senord: <input id="login_password" type="password" /></span>' +
+            '</div>');
 
-        $('#loginbutton').click(function() {
+        $('#loginbutton').click(function () {
             ultratravelUserData = new UserData();
             ultratravelUserData.loggedin = true;
             ultratravelUserData.username = $('#login_username').val();
@@ -149,15 +151,15 @@ $(document).ready(function() {
             }
 
             $('#login_popup').remove();
-            $('#login').append('<div id="login_menu">'
-                    + '<input id="saveUser" type="button" value="Spara" />'
-                    + '<input id="forgetUser" type="button" value="Glöm" />'
-                    + '</div>');
+            $('#login').append('<div id="login_menu">' +
+                    '<input id="saveUser" type="button" value="Spara" />' +
+                    '<input id="forgetUser" type="button" value="Glöm" />' +
+                    '</div>');
 
             writeLoginInfo();
         });
 
-        $('#login_password').keypress(function(evt) {
+        $('#login_password').keypress(function (evt) {
             if (evt.which === 13) {
                 ultratravelUserData = new UserData();
                 ultratravelUserData.loggedin = true;
@@ -169,10 +171,10 @@ $(document).ready(function() {
                 }
 
                 $('#login_popup').remove();
-                $('#login').append('<div id="login_menu">'
-                        + '<input id="saveUser" type="button" value="Spara" />'
-                        + '<input id="forgetUser" type="button" value="Glöm" />'
-                        + '</div>');
+                $('#login').append('<div id="login_menu">' +
+                        '<input id="saveUser" type="button" value="Spara" />' +
+                        '<input id="forgetUser" type="button" value="Glöm" />' +
+                        '</div>');
                 $('#login_menu input').hide();
 
                 writeLoginInfo();
@@ -183,19 +185,21 @@ $(document).ready(function() {
 
 
 function codeAddress(index,address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
+    var markerIcon, markerShadow, p1, p2;
+
+    geocoder.geocode( { 'address': address}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
             if (markers[index]) {
                 markers[index].setMap(null);
             }
 
             // Make a nice marker yeah
-            var markerIcon = new google.maps.MarkerImage('images/marker_mint_s.png',
+            markerIcon = new google.maps.MarkerImage('images/marker_mint_s.png',
             new google.maps.Size(31,40),    // Size
             new google.maps.Point(0,0),    // Origin
             new google.maps.Point(16,40));    // Anchor
 
-            var markerShadow = new google.maps.MarkerImage('images/marker_shadow_s.png',
+            markerShadow = new google.maps.MarkerImage('images/marker_shadow_s.png',
             new google.maps.Size(45,31),
             new google.maps.Point(0,0),
             new google.maps.Point(16,31));
@@ -214,8 +218,8 @@ function codeAddress(index,address) {
                 line.setMap(null);
             }
 
-            var p1 = markers[0].getPosition();
-            var p2 = markers[1].getPosition();
+            p1 = markers[0].getPosition();
+            p2 = markers[1].getPosition();
 
             line = new google.maps.Polyline({
                 map: map,
@@ -232,33 +236,34 @@ function codeAddress(index,address) {
 }
 
 function gotoSection(number) {
-    var $section = $(".section:nth-child("+number+")");
+    var $section, hotelPrice, expand, collapse;
 
-    if($section.height() > minimized) {
+    $section = $(".section:nth-child("+number+")");
+
+    if ($section.height() > minimized) {
         return;
     }
 
     // if the flight section is selected
-    if(number === 2) {
-
+    if (number === 2) {
         createFlights();
     }
 
     // if the boende section is selected
-    if(number === 4) {
-        if($section.children("p").children("input").attr("checked")) {
+    if (number === 4) {
+        if ($section.children("p").children("input").attr("checked")) {
             $section.children(".nextbutton").css("visibility","visible");
             disableSectionsFrom(4, true);
         }
     }
 
     // if the betalning section is selected
-    if(number === 5){
+    if (number === 5){
         $("#payment").html("<h2>" + selectedFlight.from + " - " + selectedFlight.to +"</h2>"+flight(selectedFlight));
-        var hotelPrice = 0;
+        hotelPrice = 0;
 
         // Display a summary of the booking if hotel
-        if(!$section.prev().children("p").children("input").attr("checked")) {
+        if (!$section.prev().children("p").children("input").attr("checked")) {
             $("#payment").append(hotelStay(selectedHotel));
             hotelPrice = selectedHotel.price;
         }
@@ -270,12 +275,12 @@ function gotoSection(number) {
     $(".active_section").removeClass("active_section");
     $section.children("h1").addClass("active_section");
 
-    var expand = {};
-    expand["height"] = maximized;
+    expand = {};
+    expand.height = maximized;
     $section.animate(expand, 300, null);
 
-    var collapse = {};
-    collapse["height"] = minimized;
+    collapse = {};
+    collapse.height = minimized;
     $(".section").not($section).animate(collapse, 300, null);
 
     // Hide calendar
@@ -283,26 +288,27 @@ function gotoSection(number) {
 }
 
 function createFlights() {
-    var $flights = $("#flights");
-    var $section = $(".section:nth-child(2)");
-    var from = $("#fromaddress").val();
-    var to = $("#toaddress").val();
+    var $flights, $section, from, to;
+    $flights = $("#flights");
+    $section = $(".section:nth-child(2)");
+    from = $("#fromaddress").val();
+    to = $("#toaddress").val();
 
-    if(selectedFlight.from !== from || selectedFlight.to !== to) {
+    if (selectedFlight.from !== from || selectedFlight.to !== to) {
         $flights.html('<p>Flights från <b>'+from+'</b> till <b>'+to+'</b></p>');
 
         currentFlights = new Array(3);
         for(i = 0; i < currentFlights.length; i ++) {
             currentFlights[i] = createRandomFlight();
-            $flights.append('<p class="flight">'
-                +currentFlights[i].time.start.h+':'+currentFlights[i].time.start.m
-                +' - '+currentFlights[i].time.end.h+':'+currentFlights[i].time.end.m
-                +'<br/>'+currentFlights[i].airline
-                +'<br/>'+currentFlights[i].aircraft
-                +'<br/>'+currentFlights[i].price+':-'
-                +'</p>');
+            $flights.append('<p class="flight">' +
+                currentFlights[i].time.start.h + ':' + currentFlights[i].time.start.m +
+                ' - ' + currentFlights[i].time.end.h + ':' + currentFlights[i].time.end.m +
+                '<br/>' + currentFlights[i].airline +
+                '<br/>' + currentFlights[i].aircraft +
+                '<br/>' + currentFlights[i].price + ':-' +
+                '</p>');
         }
-        $(".flight").click(function() {
+        $(".flight").click(function () {
             $(".flight").not(this).css("background","#fff");
             $(this).css("background",selectionColor);
             $section.children(":first").addClass("approved");
@@ -318,20 +324,24 @@ function createFlights() {
             disableSectionsFrom(2, true);
             $(".fseat").css("background","#00f");
         });
-    } else {
-
     }
 }
 
 function generateFlightSeats() {
-    var $section = $(".section:nth-child(3)");
-    var $flightseats = $("#flightseats");
-    var $fsection;
-    var $frow;
-    var $fcol;
-    var i, j, k, l;
+    var $section, $flightseats, $fsection, $frow, $fcol, i, j, k, l, fn;
+    $section = $(".section:nth-child(3)");
+    $flightseats = $("#flightseats");
 
-    for(i = 0; i < 2; i += 1) {
+    fn = function () {
+        $(".fseat").not(this).css("background","#00f");
+        $(this).css("background","#f00");
+
+        $section.children(":first").addClass("approved");
+        $section.next().children(":first").addClass("enabled");
+        $section.children(".nextbutton").css("visibility","visible");
+    };
+
+    for (i = 0; i < 2; i += 1) {
         $flightseats.append("<div class='fsection'></div>");
         $fsection = $flightseats.children(":nth-child("+(i+1)+")");
         for(j = 0; j < 4; j += 1) {
@@ -342,14 +352,7 @@ function generateFlightSeats() {
                 $fcol = $frow.children(":nth-child("+(k+1)+")");
                 for(l = 0; l < 9; l += 1) {
                     $fcol.append("<div class='fseat'></div>");
-                    $fcol.children(":nth-child("+(l+1)+")").click(function() {
-                        $(".fseat").not(this).css("background","#00f");
-                        $(this).css("background","#f00");
-
-                        $section.children(":first").addClass("approved");
-                        $section.next().children(":first").addClass("enabled");
-                        $section.children(".nextbutton").css("visibility","visible");
-                    });
+                    $fcol.children(":nth-child("+(l+1)+")").click(fn);
                 }
             }
         }
@@ -359,7 +362,7 @@ function generateFlightSeats() {
 function firstSectionChange() {
     var $section = $(".section:nth-child(1)");
 
-    if($("#fromaddress").val() !== "" && $("#toaddress").val() !== "") {
+    if ($("#fromaddress").val() !== "" && $("#toaddress").val() !== "") {
         disableSectionsFrom(1, true);
         $section.children("h1").addClass("approved");
         $section.next().children("h1").addClass("enabled");
@@ -369,15 +372,15 @@ function firstSectionChange() {
         $section.next().children("h1").removeClass("enabled");
         $section.children(".nextbutton").css("visibility","hidden");
     }
-};
+}
 
 function disableSectionsFrom(number, approved) {
-    if(number > 1) {
+    if (number > 1) {
         $(".section:gt("+(number-2)+")").children("h1").removeClass("approved");
     } else {
         $(".section").children("h1").removeClass("approved");
     }
-    if(approved) {
+    if (approved) {
         $(".section:nth-child("+number+")").children("h1").addClass("approved");
     }
     $(".section:gt("+number+")").children("h1").removeClass("enabled");
@@ -386,10 +389,22 @@ function disableSectionsFrom(number, approved) {
 
 // updates the boende section
 function updateBoende(checked) {
-    var $section = $(".section:nth-child(4)");
-    var $hotels = $("#hotels");
+    var $section, $hotels, fn;
+    fn = function () {
+        $hotels.children("p").not(this).css("background","#fff");
+        $(this).css("background",selectionColor);
 
-    if(checked) {
+        $section.children(":first").addClass("approved");
+        $section.next().children(":first").addClass("enabled");
+        $section.children(".nextbutton").css("visibility","visible");
+
+        selectedHotel = currentHotels[$hotels.children("p").index(this)];
+    };
+
+    $section = $(".section:nth-child(4)");
+    $hotels = $("#hotels");
+
+    if (checked) {
         disableSectionsFrom(4, true);
         $hotels.hide();
         $section.children(".nextbutton").css("visibility","visible");
@@ -400,19 +415,10 @@ function updateBoende(checked) {
         $hotels.html("");
 
         currentHotels = new Array(4);
-        for(i = 0; i<currentHotels.length; i+=1){
+        for (i = 0; i<currentHotels.length; i+=1) {
             currentHotels[i] = createRandomHotel();
             $hotels.append(hotelStay(currentHotels[i]));
-            $hotels.children(":nth-child("+(i+1)+")").click(function() {
-                $hotels.children("p").not(this).css("background","#fff");
-                $(this).css("background",selectionColor);
-
-                $section.children(":first").addClass("approved");
-                $section.next().children(":first").addClass("enabled");
-                $section.children(".nextbutton").css("visibility","visible");
-
-                selectedHotel = currentHotels[$hotels.children("p").index(this)];
-            });
+            $hotels.children(":nth-child("+(i+1)+")").click(fn);
         }
 
         $hotels.show();
